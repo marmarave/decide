e066b7631253e7cf93291bce3ebf4ac6d7619316
marmarave <73103929+marmarave@users.noreply.github.com>
2021-12-15 11:51:25 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 
-BASEURL = 'https://decide1040.herokuapp.com'
+BASEURL = 'https://decide-1040.herokuapp.com/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
